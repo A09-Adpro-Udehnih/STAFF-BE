da01a8164c23757d1f336c6c0ac783221148f7e7
Ilham Satya Nusabhakti <ilhamsn2021@gmail.com>
2025-05-01 12:07:32 +0700

    [GREEN] implement Payement Model and enums (source from PAYMENT-BE repo)

---
+++ src/main/java/com/example/staffbe/enums/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.example.staffbe.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    PAID,
+    FAILED
+}
+
---
+++ src/main/java/com/example/staffbe/model/Payment.java
@@ -0,0 +1,50 @@
+package com.example.staffbe.model;
+
+import com.example.staffbe.enums.PaymentMethod;
+import com.example.staffbe.enums.PaymentStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod method;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    private String paymentReference;
+
+    private LocalDateTime createdAt;
+}
