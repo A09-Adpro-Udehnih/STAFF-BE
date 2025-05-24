1027fe38814b5670d3b51ef9189d261f928fdb53
TrayMachi <tristan.agra@ui.ac.id>
2025-05-25 06:47:11 +0700

    fix: failed context test

--- src/test/java/com/example/staffbe/StaffBeApplicationTests.java
+++ src/test/java/com/example/staffbe/StaffBeApplicationTests.java
@@ -2,8 +2,13 @@ package com.example.staffbe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.flyway.enabled=false",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class StaffBeApplicationTests {
 
 	@Test
