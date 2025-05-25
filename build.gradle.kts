4da7cfab139b2c26e46729be85259d8725bc4a4f
Muhammad Fadhlan Karimuddin <fadhlankarimuddin@gmail.com>
2025-05-25 14:25:34 +0700

    feat/jwt-integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,13 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testRuntimeOnly("com.h2database:h2")
 
+	testRuntimeOnly("com.h2database:h2")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<Test> {
